84b6cbd14d85b498738dce8c9a5be9c4caf20f06
MUSTARDTIGER <mustardtiger@thetacoduck.com>
2023-07-13 12:57:32 -0700

    Clean up helpers

--- src/lib/Helpers.h
+++ src/lib/Helpers.h
@@ -5,12 +5,6 @@
 #include "main.h"
 
 #define M_PI 3.14159265358979323846
-double deg2rad(double deg);
-double rad2deg(double rad);
-double gpsDistanceBetween(double lat1d, double lon1d, double lat2d, double lon2d);
-double gpsCourseTo(double lat1, double long1, double lat2, double long2);
-int count_peers(bool active, config_t *cfg);
-void reset_peers();
 void pick_id();
 void resync_tx_slot(int16_t delay);
 uint8_t crc8_dvb_s2(uint8_t crc, unsigned char a);
