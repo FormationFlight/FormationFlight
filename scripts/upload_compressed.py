391a2fea11cd532bf9f242bbd1886bde74f66dc0
MUSTARDTIGER <mustardtiger@thetacoduck.com>
2023-07-20 15:52:09 -0700

    Improve upload script

--- scripts/upload_compressed.py
+++ scripts/upload_compressed.py
@@ -4,11 +4,10 @@ import os
 import requests
 import sys
 import io
-
-ENDPOINT_URL = 'http://192.168.4.1'
+import argparse
 
 def determine_platform():
-    response = requests.get(ENDPOINT_URL + "/system/status")
+    response = requests.get(args.endpoint_url_base + "/system/status")
     if response.status_code == 200:
         return response.json()['platform']
     return 'unknown'
@@ -48,19 +47,26 @@ def post_gzipped_file(url, filename, file_buffer):
     return response
 
 if __name__ == "__main__":
-    input_file_path = sys.argv[1]
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('input_file_path', type=str, nargs=1, help='a .bin file to flash')
+    parser.add_argument('-c', '--force-compression', action='store_true', help='force the binary to be gzip compressed')
+    parser.add_argument('-e', '--endpoint-url-base', default='http://192.168.4.1', help='a URL base to the flashed device, with no ending / or path')
+    args = parser.parse_args()
+
     print(f"Determining target...")
     platform = determine_platform()
     print(f"Connected to a(n) {platform}")
     # Read in the file as written
+    input_file_path = args.input_file_path[0]
     file_buffer = read_to_memory(input_file_path)
-    if platform == 'ESP8266':
+    if args.force_compression or platform == 'ESP8266':
         # Gzip compress the input file and keep it in memory
         file_buffer = gzip_compress_in_memory(input_file_path)
-    print(f"Sending {os.path.basename(input_file_path)} ({file_buffer.getbuffer().nbytes} bytes) to {ENDPOINT_URL}...")
+    print(f"Sending {os.path.basename(input_file_path)} ({file_buffer.getbuffer().nbytes} bytes) to {args.endpoint_url_base}...")
 
     # Make the HTTP POST request
-    response = post_gzipped_file(ENDPOINT_URL + '/update', "firmware.bin", file_buffer)
+    response = post_gzipped_file(args.endpoint_url_base + '/update', "firmware.bin", file_buffer)
 
     # Check the response
     if response.status_code == 200:
