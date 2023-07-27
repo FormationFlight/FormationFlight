5d84861d8c1dc63ea4f32e7115c335d90e06884a
MUSTARDTIGER <mustardtiger@thetacoduck.com>
2023-07-27 08:39:47 -0700

    Adding some documentation changes

--- scripts/build_flags.py
+++ scripts/build_flags.py
@@ -1,3 +1,7 @@
+'''
+Adds dynamic build define flags to embed git data and environment information into the firmware
+'''
+
 Import("env")
 import datetime
 import os
@@ -28,8 +32,10 @@ def get_git_hash():
     git_hash_string = "unknown"
     git_hash_result = subprocess.run("git rev-parse --short HEAD", capture_output=True, encoding='utf-8', shell=True)
     if git_hash_result.returncode == 0:
-        git_hash_result = git_hash_result.stdout
-    return git_hash_result    
+        git_hash_string = git_hash_result.stdout
+    return git_hash_string
+
+ 
 build_flags = env.get('BUILD_FLAGS', [])
 build_flags.append(format_string_define("VERSION", get_git_version()))
 build_flags.append(format_string_define("GITHASH", get_git_hash()))
